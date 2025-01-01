c235aae43ebe7245a87c298fe9d90fda7e5f8612
Justin Yen <justzyen@gmail.com>
2024-12-31 20:40:21 -0800

    Got vite-express set up

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,11 +2,11 @@ import express from "express";
 import ViteExpress from "vite-express";
 
 const app = express();
+//ViteExpress.config({ mode: "production" })
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
-
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
